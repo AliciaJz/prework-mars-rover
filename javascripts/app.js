84fcceda5b0d90ed01aaba60def25734b3194529
AliciaJz <zilaphono@gmail.com>
2018-01-12 10:25:23 -0600

    Cleaner code

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,98 +1,98 @@
 // Rover Object Goes Here
 rover = {
-  direction: "N",
+  direction: 'N',
   x: 0,
   y: 0,
 };
 
+//
+// var currentPosition = rover.x + rover.y;
+// function currentPosition() {
+//     return rover.x + rover.y;
+// }
 // Turn Left
 function turnLeft(rover) {
   switch (rover.direction) {
-    case "N":
-      rover.direction = "W";
+    case 'N':
+      rover.direction = 'W';
       break;
-    case "W":
-      rover.direction = "S";
+    case 'W':
+      rover.direction = 'S';
       break;
-    case "S":
-      rover.direction = "E";
+    case 'S':
+      rover.direction = 'E';
       break;
-    case "E":
-      rover.direction = "N";
+    case 'E':
+      rover.direction = 'N';
       break;
   }
 
-  console.log("turnLeft was called!");
-  console.log("You are now facing " + rover.direction);
+  console.log('turnLeft was called!');
+  console.log('You are now facing ' + rover.direction);
 
 }
 
 // Turn Right
 function turnRight(rover) {
   switch (rover.direction) {
-    case "N":
-      rover.direction = "E";
+    case 'N':
+      rover.direction = 'E';
       break;
-    case "E":
-      rover.direction = "S";
+    case 'E':
+      rover.direction = 'S';
       break;
-    case "S":
-      rover.direction = "W";
+    case 'S':
+      rover.direction = 'W';
       break;
-    case "W":
-      rover.direction = "N";
+    case 'W':
+      rover.direction = 'N';
       break;
   }
 
-  console.log("turnRight was called!");
-  console.log("You are now facing " + rover.direction);
-
+  console.log('turnRight was called!');
+  console.log('You are now facing ' + rover.direction);
 }
-
 
 // Move Forward
 function moveForward(rover) {
-if (rover.direction === "N") {
-  rover.y -= 1;
-}
-else if (rover.direction === "W") {
-  rover.x -= 1;
-}
-else if (rover.direction === "S") {
-  rover.y += 1;
-}
-else if (rover.direction === "E") {
-  rover.x += 1;
-}
-else {
-  console.log("The Rover is in the same place")
-}
+  if (rover.direction === 'N') {
+    rover.y -= 1;
+  } else if (rover.direction === 'W') {
+    rover.x -= 1;
+  } else if (rover.direction === 'S') {
+    rover.y += 1;
+  } else if (rover.direction === 'E') {
+    rover.x += 1;
+  } else {
+    console.log('The Rover is in the same place');
+  }
 
-  console.log("moveForward was called")
-  console.log("You are still facing " + rover.direction);
+  console.log('moveForward was called');
+  console.log('You are still facing ' + rover.direction);
   rover.position =
-  console.log("And your new position is: " + "(" + rover.x + "," + rover.y + ")");
+    console.log('And your new position is: ' + '(' + rover.x + ',' + rover.y + ')');
 }
 
 // Move Backward
 function moveBackward(rover) {
-  if (rover.direction === "N") {
+  if (rover.direction === 'N') {
     rover.y += 1;
-  }
-  else if (rover.direction === "W") {
+  } else if (rover.direction === 'W') {
     rover.x += 1;
-  }
-  else if (rover.direction === "S") {
+  } else if (rover.direction === 'S') {
     rover.y -= 1;
-  }
-  else if (rover.direction === "E") {
+  } else if (rover.direction === 'E') {
     rover.x -= 1;
-  }
-  else {
-    console.log("The Rover is in the same place")
+  } else {
+    console.log('The Rover is in the same place');
   }
 
-  console.log("moveBackward was called")
-  console.log("You are still facing " + rover.direction);
-  console.log("And your new position is: " + "(" + rover.x + "," + rover.y + ")");
+  console.log('moveBackward was called');
+  console.log('You are still facing ' + rover.direction);
+  console.log('And your new position is: ' + '(' + rover.x + ',' + rover.y + ')');
 }
+
+// COMMANDS
+
+// function commands()
+// var commands = 'rffrfflfrff';
