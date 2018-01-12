99812d3d629e4de5213e1b196129dd958243c14d
AliciaJz <zilaphono@gmail.com>
2018-01-12 09:11:30 -0600

    changed position var and add moveForward loop

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,12 @@
 // Rover Object Goes Here
-// ======================
 rover = {
   direction: "N",
-  position: {
-    x: 0,
-    y: 0
-  }
-}
-// ======================
+  x: 0,
+  y: 0,
+};
 
-function turnLeft(rover){
+// Turn Left
+function turnLeft(rover) {
   switch (rover.direction) {
     case "N":
       rover.direction = "W";
@@ -30,7 +27,8 @@ function turnLeft(rover){
 
 }
 
-function turnRight(rover){
+// Turn Right
+function turnRight(rover) {
   switch (rover.direction) {
     case "N":
       rover.direction = "E";
@@ -51,33 +49,34 @@ function turnRight(rover){
 
 }
 
-// Mars Matrix
-// ======================
-// var mars = [
-//   [null, null, null, null, null, null, null, null, null, null],
-//   [null, null, null, null, null, null, null, null, null, null],
-//   [null, null, null, null, null, null, null, null, null, null],
-//   [null, null, null, null, null, null, null, null, null, null],
-//   [null, null, null, null, null, null, null, null, null, null],
-//   [null, null, null, null, null, null, null, null, null, null],
-//   [null, null, null, null, null, null, null, null, null, null],
-//   [null, null, null, null, null, null, null, null, null, null],
-//   [null, null, null, null, null, null, null, null, null, null],
-//   [null, null, null, null, null, null, null, null, null, null],
-//
-// ];
 
-// ======================
-
-function moveForward(rover){
+// Move Forward
+function moveForward(rover) {
+if (rover.direction === "N") {
+  rover.y -= 1;
+}
+else if (rover.direction === "W") {
+  rover.x -= 1;
+}
+else if (rover.direction === "S") {
+  rover.y += 1;
+}
+else if (rover.direction === "E") {
+  rover.x += 1;
+}
+else {
+  console.log("The Rover is in the same place")
+}
 
   console.log("moveForward was called")
   console.log("You are still facing " + rover.direction);
-  console.log("Your position is " + rover.position);
+  rover.position =
+  console.log("And your new position is: " + "(" + rover.x + "," + rover.y + ")");
 }
 
-function moveBackward(rover){
+// Move Backward
+function moveBackward(rover) {
   console.log("moveBackward was called")
   console.log("You are still facing " + rover.direction);
-  console.log("Your position is " + rover.position);
+  console.log("And your new position is " + rover.position);
 }
